80c6e73eb74f93dbcefd3bbbd980c586b090687b
sensura-san <gohzhixuan2006@gmail.com>
2025-11-08 22:14:20 +0700

    fix model dir

--- quickstart.sh
+++ quickstart.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 cd /media/ic2/ESD-USB/llm-bench || exit
 export LLAMA="/home/ic2/llm-bench/llama.cpp/build/bin/llama-bench"
-export MODEL_DIR="/media/ic2/ESD-USB/models"
+export MODEL_DIR="/media/ic2/ESD-USB/llm-bench/models"
 bash ./llm-bench
