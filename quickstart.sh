772a5f45b1962214dbbcf883258f5d9b3eab7cee
sensura-san <gohzhixuan2006@gmail.com>
2025-11-08 22:08:30 +0700

    update quickstart.sh

--- quickstart.sh
+++ quickstart.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 cd /media/ic2/ESD-USB/llm-bench || exit
-LLAMA="/home/ic2/llm-bench/llama.cpp/build/bin/llama-bench" bash ./llm-bench
+export LLAMA="/home/ic2/llm-bench/llama.cpp/build/bin/llama-bench"
+export MODEL_DIR="/media/ic2/ESD-USB/models"
+bash ./llm-bench
